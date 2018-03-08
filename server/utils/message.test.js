8dbabd4efe579c104292a44f9957044e389eb644
Digotetso Matema <dee@Digotetsos-MacBook-Air.local>
2018-03-08 16:48:51 +0200

    test case

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,7 +1,7 @@
 var expect = require('expect')
 
 
-var {generateMessage} = require('./message')
+var {generateMessage, generateLocationMessage} = require('./message')
 
 
 describe('generate message', () => {
@@ -17,4 +17,23 @@ describe('generate message', () => {
             expect(message).toInclude({from, text});
             expect(message.createdAt).toBeA('number')
         })
+})
+
+
+describe('generate Location message', () => {
+    it('it should generate correct location object', () => {
+        var latitude = -24.2;
+        var longitude = 25.5;
+        var from = 'Admin';
+        var url = `https://www.google.com/maps?q=-24.2,25.5`
+
+        var locationMessage = generateLocationMessage(from, latitude, longitude)
+
+        expect(locationMessage.createdAt).toBeA('number')
+        expect(locationMessage.url).toBe(`https://www.google.com/maps?q=-24.2,25.5`)
+        expect(locationMessage).toInclude({from,url})
+
+
+
+    })
 })
